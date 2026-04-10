8ecaaf671822caa64b729b44f702512f0b702049
Maestro00Manuel <u5723104509@gmail.com>
2026-04-10 21:31:43 +0200

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         return res.status(500).json({ error: { message: 'Falta la API Key en Vercel.' } });
     }
 
-    const model = req.query.model || 'gemini-2.5-flash';
+    const model = req.query.model || 'gemini-1.5-flash';
     const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
 
     try {
